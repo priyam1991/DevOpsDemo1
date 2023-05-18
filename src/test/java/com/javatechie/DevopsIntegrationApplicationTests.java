868e5d38cf0fef983a657ca991006d0e47f65af7
ampatil <anilmpatil27@gmail.com>
2023-05-18 11:50:24 +0530

    Update DevopsIntegrationApplicationTests.java

--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -3,7 +3,7 @@ package com.javatechie;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/* @SpringBootTest  */
 class DevopsIntegrationApplicationTests {
 
 	@Test
