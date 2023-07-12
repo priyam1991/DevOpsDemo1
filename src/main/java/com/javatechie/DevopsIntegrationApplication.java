4d5156a47f9b50bc534a5c23fe03c0903dbcdc1a
ampatil <anilmpatil27@gmail.com>
2023-07-12 14:07:56 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DevopsIntegrationApplication {
 
-	@GetMapping
+	@GetMapping(value = "/message")
 	public String message(){
 		return "welcome to devops";
 	}
